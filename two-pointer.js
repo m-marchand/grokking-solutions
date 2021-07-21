f0dc083fdd5465bc007e76cc5dad9f37b2a8fed3
Matthew <matthew.marchand.93@gmail.com>
2021-07-21 15:21:07 -0700

    create files for two-pointer and fast-slow patterns

---
+++ two-pointer.js
@@ -0,0 +1,24 @@
+// DUTCH NATIONAL FLAG PROBLEM
+const dutch_flag_sort = function (arr) {
+  // init left and right pointers
+  // iterate over the input arr: while left !== right
+  // if we encounter a 2 place it at the end
+  // if we encounter a 0, place at the beginning
+};
+
+let arr = [1, 0, 2, 1, 0];
+dutch_flag_sort(arr);
+console.log(arr);
+
+arr = [2, 2, 0, 1, 2, 0];
+dutch_flag_sort(arr);
+console.log(arr);
+
+
+
+
+
+
+
+
+
